a98a262de7d4249354258a78165298295309e546
ybro2011 <ybro2011@gmail.com>
2025-05-24 17:48:38 +0100

    Add allowed hosts configuration for Render deployment

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+    allowedHosts: ['onlydown.onrender.com']
+  }
 }) 
